a8600506d85b9388c59e0c814b90d9ab0a7edd06
oTrinhHoangHai <trinh.hoang.hai@framgia.com>
2019-03-05 15:03:37 +0700

    bleh

--- StackViewVsScrollView/ViewController.swift
+++ StackViewVsScrollView/ViewController.swift
@@ -12,9 +12,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setupConstraints()
+
     }
 
+    func setupConstraints() {
 
+        let redView = UIView()
+        let orangeView = UIView()
+        let blueView = UIView()
+        let greenView = UIView()
+
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        orangeView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+
+        redView.backgroundColor = .red
+        orangeView.backgroundColor = .orange
+        blueView.backgroundColor = .blue
+        greenView.backgroundColor = .green
+
+        self.view.addSubview(redView)
+        self.view.addSubview(orangeView)
+        self.view.addSubview(blueView)
+        self.view.addSubview(greenView)
+
+        redView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+        redView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        redView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+
+
+        orangeView.firstBaselineAnchor.constraint(equalTo: redView.firstBaselineAnchor).isActive = true
+        orangeView.lastBaselineAnchor.constraint(equalTo: redView.lastBaselineAnchor).isActive = true
+        orangeView.leadingAnchor.constraint(equalTo: redView.trailingAnchor, constant: 2).isActive = true
+        orangeView.widthAnchor.constraint(equalToConstant: 40)
+
+
+        greenView.firstBaselineAnchor.constraint(equalTo: redView.firstBaselineAnchor).isActive = true
+        greenView.lastBaselineAnchor.constraint(equalTo: redView.lastBaselineAnchor).isActive = true
+        greenView.leadingAnchor.constraint(equalTo: redView.trailingAnchor, constant: -2).isActive = true
+        greenView.widthAnchor.constraint(equalToConstant: 40)
+
+
+        blueView.leadingAnchor.constraint(equalTo: redView.leadingAnchor).isActive = true
+        blueView.trailingAnchor.constraint(equalTo: redView.trailingAnchor).isActive = true
+        blueView.bottomAnchor.constraint(equalTo: redView.topAnchor, constant: 20)
+        blueView.heightAnchor.constraint(equalToConstant: 40)
+
+
+
+    }
 }
 
